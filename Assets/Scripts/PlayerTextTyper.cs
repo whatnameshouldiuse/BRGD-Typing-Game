e97cf8f2c36be9b9d56cdf1f0567c2cc8d9c39e3
tobybartclev <56485540+tobybartclev@users.noreply.github.com>
2021-02-13 16:54:09 -0500

    Small changes to text typing script

--- Assets/Scripts/PlayerTextTyper.cs
+++ Assets/Scripts/PlayerTextTyper.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerTextTyper : MonoBehaviour
 {
+    public string textTyped;    //The text typed by the player
+    public TextMeshProUGUI textBox; //The text display (provisional, may be implemented permanently)
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textBox = this.gameObject.GetComponent<TextMeshProUGUI>();  //Get text box
+        textBox.text = "Sample Text";                           //Placeholder text
     }
 
     // Update is called once per frame
